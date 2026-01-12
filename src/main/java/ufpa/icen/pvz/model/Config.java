f2e40df6e7b1fb910dc9c5e431a7f7176b30ca4a
reali-705 <alessandro.reali.3110@gmail.com>
2026-01-12 20:01:19 -0300

    docs: melhorias sugeridas pelo copilot

--- src/main/java/ufpa/icen/pvz/model/Config.java
+++ src/main/java/ufpa/icen/pvz/model/Config.java
@@ -11,21 +11,21 @@ package ufpa.icen.pvz.model;
  * </p>
  */
 public class Config {
-    // tabuleiro
+    // Configurações do Tabuleiro
     public static final int LARGURA_TABULEIRO = 9;
     public static final int ALTURA_TABULEIRO = 5;
     
-    // recursos
+    // Configurações de Recursos
     public static final int RECURSOS_INICIAIS = 50;
     public static final int RECURSOS_POR_CICLO = 25;
     public static final int CUSTO_ATIRADOR = 50;
 
-    // tempo em milissegundos
+    // Configurações de Tempo (milissegundos)
     public static final int TEMPO_CICLO = 1000;
     public static final int TEMPO_DISPARO = 1000;
     public static final int TEMPO_SPAWN_ZUMBIS = 3000;
 
-    // entidades
+    // Configurações de Entidades
     public static final int VIDA_ZUMBI = 100;
     public static final int DANO_ZUMBI = 10;
     public static final double VELOCIDADE_ZUMBI = 0.5;
